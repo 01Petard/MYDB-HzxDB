835afd34c90e3a3ca298deca6a385c620ee53d05
01Petard <1520394133@qq.com>
2024-09-17 00:59:14 +0800

    fix: 修复tm包的注释

---
+++ src/main/java/top/guoziyang/mydb/backend/tm/package-info.java
@@ -0,0 +1,5 @@
+package top.guoziyang.mydb.backend.tm;
+/*
+  事务管理器
+  https://blog.csdn.net/qq_40856284/article/details/121594780
+ */
--- src/main/java/top/guoziyang/mydb/backend/vm/Transaction.java
+++ src/main/java/top/guoziyang/mydb/backend/vm/Transaction.java
@@ -1,25 +1,43 @@
 package top.guoziyang.mydb.backend.vm;
 
+import top.guoziyang.mydb.backend.tm.TransactionManagerImpl;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import top.guoziyang.mydb.backend.tm.TransactionManagerImpl;
-
-// vm对一个事务的抽象
+/**
+ * 事务类，由VM层调用
+ * 每个事务对应一个Transaction对象
+ */
 public class Transaction {
+    /**
+     * 事务的唯一xid
+     */
     public long xid;
+    /**
+     * 事务的隔离级别，0:READ_COMMITTED，1:REPEATABLE_READ
+     */
     public int level;
+    /**
+     * 快照
+     */
     public Map<Long, Boolean> snapshot;
+    /**
+     * 错误信息
+     */
     public Exception err;
+    /**
+     * 是否自动回滚
+     */
     public boolean autoAborted;
 
     public static Transaction newTransaction(long xid, int level, Map<Long, Transaction> active) {
         Transaction t = new Transaction();
         t.xid = xid;
         t.level = level;
-        if(level != 0) {
+        if (level != 0) {
             t.snapshot = new HashMap<>();
-            for(Long x : active.keySet()) {
+            for (Long x : active.keySet()) {
                 t.snapshot.put(x, true);
             }
         }
@@ -27,7 +45,7 @@ public class Transaction {
     }
 
     public boolean isInSnapshot(long xid) {
-        if(xid == TransactionManagerImpl.SUPER_XID) {
+        if (xid == TransactionManagerImpl.SUPER_XID) {
             return false;
         }
         return snapshot.containsKey(xid);
