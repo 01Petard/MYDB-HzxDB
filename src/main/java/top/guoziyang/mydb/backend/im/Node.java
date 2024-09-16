c10b0df91a469e4b8fea60cc905c839289d3803d
01Petard <1520394133@qq.com>
2024-09-17 01:02:55 +0800

    fix: 修复im包的注释

--- src/main/java/top/guoziyang/mydb/backend/im/Node.java
+++ src/main/java/top/guoziyang/mydb/backend/im/Node.java
@@ -1,95 +1,126 @@
 package top.guoziyang.mydb.backend.im;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import top.guoziyang.mydb.backend.common.SubArray;
 import top.guoziyang.mydb.backend.dm.dataItem.DataItem;
 import top.guoziyang.mydb.backend.tm.TransactionManagerImpl;
 import top.guoziyang.mydb.backend.utils.Parser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Node结构如下：
  * [LeafFlag][KeyNumber][SiblingUid]
  * [Son0][Key0][Son1][Key1]...[SonN][KeyN]
+ * LeafFlag 标记了该节点是否是个叶子节点；
+ * KeyNumber 为该节点中 key 的个数；
+ * SiblingUid 是其兄弟节点存储在 DM 中的 UID。
+ * 后续是穿插的子节点（SonN）和 KeyN。最后的一个 KeyN 始终为 MAX_VALUE，以此方便查找。
  */
 public class Node {
+    // 叶子节点标识
     static final int IS_LEAF_OFFSET = 0;
-    static final int NO_KEYS_OFFSET = IS_LEAF_OFFSET+1;
-    static final int SIBLING_OFFSET = NO_KEYS_OFFSET+2;
-    static final int NODE_HEADER_SIZE = SIBLING_OFFSET+8;
 
+    // 节点中 key 的个数
+    static final int NO_KEYS_OFFSET = IS_LEAF_OFFSET + 1;
+
+    // 兄弟节点存储在 DM 中的 UID
+    static final int SIBLING_OFFSET = NO_KEYS_OFFSET + 2;
+
+    // 子节点和 key 的偏移量
+    static final int NODE_HEADER_SIZE = SIBLING_OFFSET + 8;
+
+    // 子节点和 key 的个数
     static final int BALANCE_NUMBER = 32;
-    static final int NODE_SIZE = NODE_HEADER_SIZE + (2*8)*(BALANCE_NUMBER*2+2);
+
+    // 子节点和 key 的偏移量
+    static final int NODE_SIZE = NODE_HEADER_SIZE + (2 * 8) * (BALANCE_NUMBER * 2 + 2);
 
     BPlusTree tree;
     DataItem dataItem;
     SubArray raw;
     long uid;
 
+    /**
+     * 设置节点为叶子节点和非叶子节点
+     * @param raw
+     * @param isLeaf
+     */
     static void setRawIsLeaf(SubArray raw, boolean isLeaf) {
-        if(isLeaf) {
-            raw.raw[raw.start + IS_LEAF_OFFSET] = (byte)1;
+        if (isLeaf) {
+            raw.raw[raw.start + IS_LEAF_OFFSET] = (byte) 1;
         } else {
-            raw.raw[raw.start + IS_LEAF_OFFSET] = (byte)0;
+            raw.raw[raw.start + IS_LEAF_OFFSET] = (byte) 0;
         }
     }
 
+    /**
+     * 判断一个节点是否是叶子节点
+     * @param raw
+     * @return
+     */
     static boolean getRawIfLeaf(SubArray raw) {
-        return raw.raw[raw.start + IS_LEAF_OFFSET] == (byte)1;
+        return raw.raw[raw.start + IS_LEAF_OFFSET] == (byte) 1;
     }
 
     static void setRawNoKeys(SubArray raw, int noKeys) {
-        System.arraycopy(Parser.short2Byte((short)noKeys), 0, raw.raw, raw.start+NO_KEYS_OFFSET, 2);
+        System.arraycopy(Parser.short2Byte((short) noKeys), 0, raw.raw, raw.start + NO_KEYS_OFFSET, 2);
     }
 
     static int getRawNoKeys(SubArray raw) {
-        return (int)Parser.parseShort(Arrays.copyOfRange(raw.raw, raw.start+NO_KEYS_OFFSET, raw.start+NO_KEYS_OFFSET+2));
+        return (int) Parser.parseShort(Arrays.copyOfRange(raw.raw, raw.start + NO_KEYS_OFFSET, raw.start + NO_KEYS_OFFSET + 2));
     }
 
     static void setRawSibling(SubArray raw, long sibling) {
-        System.arraycopy(Parser.long2Byte(sibling), 0, raw.raw, raw.start+SIBLING_OFFSET, 8);
+        System.arraycopy(Parser.long2Byte(sibling), 0, raw.raw, raw.start + SIBLING_OFFSET, 8);
     }
 
     static long getRawSibling(SubArray raw) {
-        return Parser.parseLong(Arrays.copyOfRange(raw.raw, raw.start+SIBLING_OFFSET, raw.start+SIBLING_OFFSET+8));
+        return Parser.parseLong(Arrays.copyOfRange(raw.raw, raw.start + SIBLING_OFFSET, raw.start + SIBLING_OFFSET + 8));
     }
 
     static void setRawKthSon(SubArray raw, long uid, int kth) {
-        int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2);
+        int offset = raw.start + NODE_HEADER_SIZE + kth * (8 * 2);
         System.arraycopy(Parser.long2Byte(uid), 0, raw.raw, offset, 8);
     }
 
     static long getRawKthSon(SubArray raw, int kth) {
-        int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2);
-        return Parser.parseLong(Arrays.copyOfRange(raw.raw, offset, offset+8));
+        int offset = raw.start + NODE_HEADER_SIZE + kth * (8 * 2);
+        return Parser.parseLong(Arrays.copyOfRange(raw.raw, offset, offset + 8));
     }
 
     static void setRawKthKey(SubArray raw, long key, int kth) {
-        int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2)+8;
+        int offset = raw.start + NODE_HEADER_SIZE + kth * (8 * 2) + 8;
         System.arraycopy(Parser.long2Byte(key), 0, raw.raw, offset, 8);
     }
 
     static long getRawKthKey(SubArray raw, int kth) {
-        int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2)+8;
-        return Parser.parseLong(Arrays.copyOfRange(raw.raw, offset, offset+8));
+        int offset = raw.start + NODE_HEADER_SIZE + kth * (8 * 2) + 8;
+        return Parser.parseLong(Arrays.copyOfRange(raw.raw, offset, offset + 8));
     }
 
     static void copyRawFromKth(SubArray from, SubArray to, int kth) {
-        int offset = from.start+NODE_HEADER_SIZE+kth*(8*2);
-        System.arraycopy(from.raw, offset, to.raw, to.start+NODE_HEADER_SIZE, from.end-offset);
+        int offset = from.start + NODE_HEADER_SIZE + kth * (8 * 2);
+        System.arraycopy(from.raw, offset, to.raw, to.start + NODE_HEADER_SIZE, from.end - offset);
     }
 
     static void shiftRawKth(SubArray raw, int kth) {
-        int begin = raw.start+NODE_HEADER_SIZE+(kth+1)*(8*2);
-        int end = raw.start+NODE_SIZE-1;
-        for(int i = end; i >= begin; i --) {
-            raw.raw[i] = raw.raw[i-(8*2)];
+        int begin = raw.start + NODE_HEADER_SIZE + (kth + 1) * (8 * 2);
+        int end = raw.start + NODE_SIZE - 1;
+        for (int i = end; i >= begin; i--) {
+            raw.raw[i] = raw.raw[i - (8 * 2)];
         }
     }
 
-    static byte[] newRootRaw(long left, long right, long key)  {
+    /**
+     * 生成一个根节点的数据
+     * @param left
+     * @param right
+     * @param key
+     * @return
+     */
+    static byte[] newRootRaw(long left, long right, long key) {
         SubArray raw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);
 
         setRawIsLeaf(raw, false);
@@ -103,7 +134,11 @@ public class Node {
         return raw.raw;
     }
 
-    static byte[] newNilRootRaw()  {
+    /**
+     * 生成一个空的根节点数据
+     * @return
+     */
+    static byte[] newNilRootRaw() {
         SubArray raw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);
 
         setRawIsLeaf(raw, true);
@@ -137,19 +172,19 @@ public class Node {
         }
     }
 
-    class SearchNextRes {
-        long uid;
-        long siblingUid;
-    }
-
+    /**
+     * 寻找对应 key 的 UID, 如果找不到, 则返回兄弟节点的 UID
+     * @param key
+     * @return
+     */
     public SearchNextRes searchNext(long key) {
         dataItem.rLock();
         try {
             SearchNextRes res = new SearchNextRes();
             int noKeys = getRawNoKeys(raw);
-            for(int i = 0; i < noKeys; i ++) {
+            for (int i = 0; i < noKeys; i++) {
                 long ik = getRawKthKey(raw, i);
-                if(key < ik) {
+                if (key < ik) {
                     res.uid = getRawKthSon(raw, i);
                     res.siblingUid = 0;
                     return res;
@@ -164,35 +199,37 @@ public class Node {
         }
     }
 
-    class LeafSearchRangeRes {
-        List<Long> uids;
-        long siblingUid;
-    }
-
-    public LeafSearchRangeRes leafSearchRange(long leftKey, long rightKey) {
+    /**
+     * 在当前节点进行范围查找，范围是 [leftKey, rightKey]
+     * 如果 rightKey 大于等于该节点的最大的 key, 则同时返回兄弟节点的 UID，方便继续搜索下一个节点
+     * @param leftKey
+     * @param rightKey
+     * @return
+     */
+    LeafSearchRangeRes leafSearchRange(long leftKey, long rightKey) {
         dataItem.rLock();
         try {
             int noKeys = getRawNoKeys(raw);
             int kth = 0;
-            while(kth < noKeys) {
+            while (kth < noKeys) {
                 long ik = getRawKthKey(raw, kth);
-                if(ik >= leftKey) {
+                if (ik >= leftKey) {
                     break;
                 }
-                kth ++;
+                kth++;
             }
             List<Long> uids = new ArrayList<>();
-            while(kth < noKeys) {
+            while (kth < noKeys) {
                 long ik = getRawKthKey(raw, kth);
-                if(ik <= rightKey) {
+                if (ik <= rightKey) {
                     uids.add(getRawKthSon(raw, kth));
-                    kth ++;
+                    kth++;
                 } else {
                     break;
                 }
             }
             long siblingUid = 0;
-            if(kth == noKeys) {
+            if (kth == noKeys) {
                 siblingUid = getRawSibling(raw);
             }
             LeafSearchRangeRes res = new LeafSearchRangeRes();
@@ -204,11 +241,7 @@ public class Node {
         }
     }
 
-    class InsertAndSplitRes {
-        long siblingUid, newSon, newKey;
-    }
-
-    public InsertAndSplitRes insertAndSplit(long uid, long key) throws Exception {
+    InsertAndSplitRes insertAndSplit(long uid, long key) throws Exception {
         boolean success = false;
         Exception err = null;
         InsertAndSplitRes res = new InsertAndSplitRes();
@@ -216,17 +249,17 @@ public class Node {
         dataItem.before();
         try {
             success = insert(uid, key);
-            if(!success) {
+            if (!success) {
                 res.siblingUid = getRawSibling(raw);
                 return res;
             }
-            if(needSplit()) {
+            if (needSplit()) {
                 try {
                     SplitRes r = split();
                     res.newSon = r.newSon;
                     res.newKey = r.newKey;
                     return res;
-                } catch(Exception e) {
+                } catch (Exception e) {
                     err = e;
                     throw e;
                 }
@@ -234,7 +267,7 @@ public class Node {
                 return res;
             }
         } finally {
-            if(err == null && success) {
+            if (err == null && success) {
                 dataItem.after(TransactionManagerImpl.SUPER_XID);
             } else {
                 dataItem.unBefore();
@@ -245,38 +278,34 @@ public class Node {
     private boolean insert(long uid, long key) {
         int noKeys = getRawNoKeys(raw);
         int kth = 0;
-        while(kth < noKeys) {
+        while (kth < noKeys) {
             long ik = getRawKthKey(raw, kth);
-            if(ik < key) {
-                kth ++;
+            if (ik < key) {
+                kth++;
             } else {
                 break;
             }
         }
-        if(kth == noKeys && getRawSibling(raw) != 0) return false;
+        if (kth == noKeys && getRawSibling(raw) != 0) return false;
 
-        if(getRawIfLeaf(raw)) {
+        if (getRawIfLeaf(raw)) {
             shiftRawKth(raw, kth);
             setRawKthKey(raw, key, kth);
             setRawKthSon(raw, uid, kth);
-            setRawNoKeys(raw, noKeys+1);
+            setRawNoKeys(raw, noKeys + 1);
         } else {
             long kk = getRawKthKey(raw, kth);
             setRawKthKey(raw, key, kth);
-            shiftRawKth(raw, kth+1);
-            setRawKthKey(raw, kk, kth+1);
-            setRawKthSon(raw, uid, kth+1);
-            setRawNoKeys(raw, noKeys+1);
+            shiftRawKth(raw, kth + 1);
+            setRawKthKey(raw, kk, kth + 1);
+            setRawKthSon(raw, uid, kth + 1);
+            setRawNoKeys(raw, noKeys + 1);
         }
         return true;
     }
 
     private boolean needSplit() {
-        return BALANCE_NUMBER*2 == getRawNoKeys(raw);
-    }
-
-    class SplitRes {
-        long newSon, newKey;
+        return BALANCE_NUMBER * 2 == getRawNoKeys(raw);
     }
 
     private SplitRes split() throws Exception {
@@ -302,10 +331,28 @@ public class Node {
         int KeyNumber = getRawNoKeys(raw);
         sb.append("KeyNumber: ").append(KeyNumber).append("\n");
         sb.append("sibling: ").append(getRawSibling(raw)).append("\n");
-        for(int i = 0; i < KeyNumber; i ++) {
+        for (int i = 0; i < KeyNumber; i++) {
             sb.append("son: ").append(getRawKthSon(raw, i)).append(", key: ").append(getRawKthKey(raw, i)).append("\n");
         }
         return sb.toString();
+    }
+
+    static class SearchNextRes {
+        long uid;
+        long siblingUid;
+    }
+
+    static class LeafSearchRangeRes {
+        List<Long> uids;
+        long siblingUid;
+    }
+
+    static class InsertAndSplitRes {
+        long siblingUid, newSon, newKey;
+    }
+
+    static class SplitRes {
+        long newSon, newKey;
     }
 
 }
