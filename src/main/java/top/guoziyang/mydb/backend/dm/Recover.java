4961a4aaaba935cdc65c38add701718a1e01e548
01Petard <1520394133@qq.com>
2024-09-17 01:18:41 +0800

    fix: 修复Recover类的注释

--- src/main/java/top/guoziyang/mydb/backend/dm/Recover.java
+++ src/main/java/top/guoziyang/mydb/backend/dm/Recover.java
@@ -20,11 +20,23 @@ import top.guoziyang.mydb.backend.utils.Panic;
 import top.guoziyang.mydb.backend.utils.Parser;
 
 public class Recover {
-
+    /**
+     * insertLog:
+     * [LogType] [XID] [Pgno] [Offset] [Raw]
+     */
     private static final byte LOG_TYPE_INSERT = 0;
+    /**
+     * updateLog:
+     * [LogType] [XID] [UID] [OldRaw] [NewRaw]
+     */
     private static final byte LOG_TYPE_UPDATE = 1;
-
+    /**
+     * 重做日志
+     */
     private static final int REDO = 0;
+    /**
+     * 撤销日志
+     */
     private static final int UNDO = 1;
 
     static class InsertLogInfo {
@@ -42,9 +54,19 @@ public class Recover {
         byte[] newRaw;
     }
 
+    /**
+     * 故障重启
+     * @param tm
+     * @param lg
+     * @param pc
+     */
     public static void recover(TransactionManager tm, Logger lg, PageCache pc) {
         System.out.println("Recovering...");
 
+        /*
+        找到日志文件中最大的页号，并截断日志文件
+         */
+        // 截断日志文件
         lg.rewind();
         int maxPgno = 0;
         while(true) {
@@ -68,15 +90,23 @@ public class Recover {
         pc.truncateByBgno(maxPgno);
         System.out.println("Truncate to " + maxPgno + " pages.");
 
+        // 执行重做操作，回滚已完成的事务
         redoTranscations(tm, lg, pc);
         System.out.println("Redo Transactions Over.");
 
+        // 执行撤销操作，撤销未完成的事务
         undoTranscations(tm, lg, pc);
         System.out.println("Undo Transactions Over.");
 
         System.out.println("Recovery Over.");
     }
 
+    /**
+     * 重做所有已完成事务
+     * @param tm
+     * @param lg
+     * @param pc
+     */
     private static void redoTranscations(TransactionManager tm, Logger lg, PageCache pc) {
         lg.rewind();
         while(true) {
@@ -98,26 +128,42 @@ public class Recover {
         }
     }
 
+    /**
+     * 撤销所有未完成事务
+     * @param tm
+     * @param lg
+     * @param pc
+     */
     private static void undoTranscations(TransactionManager tm, Logger lg, PageCache pc) {
+        // 存储每个事务的日志列表
         Map<Long, List<byte[]>> logCache = new HashMap<>();
+
+        // 重置日志文件指针到起始位置
         lg.rewind();
-        while(true) {
+
+        // 循环读取日志直到没有更多日志可读
+        while (true) {
             byte[] log = lg.next();
-            if(log == null) break;
-            if(isInsertLog(log)) {
+            if (log == null) break; // 如果没有更多的日志，退出循环
+
+            // 如果是插入日志
+            if (isInsertLog(log)) {
                 InsertLogInfo li = parseInsertLog(log);
                 long xid = li.xid;
-                if(tm.isActive(xid)) {
-                    if(!logCache.containsKey(xid)) {
+                // 如果事务是活动状态，将日志存入缓存
+                if (tm.isActive(xid)) {
+                    if (!logCache.containsKey(xid)) {
                         logCache.put(xid, new ArrayList<>());
                     }
                     logCache.get(xid).add(log);
                 }
             } else {
+                // 如果是更新日志
                 UpdateLogInfo xi = parseUpdateLog(log);
                 long xid = xi.xid;
-                if(tm.isActive(xid)) {
-                    if(!logCache.containsKey(xid)) {
+                // 如果事务是活动状态，将日志存入缓存
+                if (tm.isActive(xid)) {
+                    if (!logCache.containsKey(xid)) {
                         logCache.put(xid, new ArrayList<>());
                     }
                     logCache.get(xid).add(log);
@@ -125,24 +171,40 @@ public class Recover {
             }
         }
 
-        // 对所有active log进行倒序undo
-        for(Entry<Long, List<byte[]>> entry : logCache.entrySet()) {
+        // 对所有活动事务的日志进行逆序撤销操作
+        for (Entry<Long, List<byte[]>> entry : logCache.entrySet()) {
             List<byte[]> logs = entry.getValue();
-            for (int i = logs.size()-1; i >= 0; i --) {
+            for (int i = logs.size() - 1; i >= 0; i--) {
                 byte[] log = logs.get(i);
-                if(isInsertLog(log)) {
+                if (isInsertLog(log)) {
                     doInsertLog(pc, log, UNDO);
                 } else {
                     doUpdateLog(pc, log, UNDO);
                 }
             }
+            // 终止事务
             tm.abort(entry.getKey());
         }
     }
 
+    /**
+     * 判断日志类型是否是插入
+     * @param log
+     * @return
+     */
     private static boolean isInsertLog(byte[] log) {
         return log[0] == LOG_TYPE_INSERT;
     }
+
+    /**
+     * 判断日志类型是否是工更新
+     * @param log
+     * @return
+     */
+    private static boolean isUpdateLog(byte[] log) {
+        return log[0] == LOG_TYPE_UPDATE;
+    }
+
 
     // [LogType] [XID] [UID] [OldRaw] [NewRaw]
     private static final int OF_TYPE = 0;
@@ -150,6 +212,12 @@ public class Recover {
     private static final int OF_UPDATE_UID = OF_XID+8;
     private static final int OF_UPDATE_RAW = OF_UPDATE_UID+8;
 
+    /**
+     * 更新一条日志
+     * @param xid
+     * @param di
+     * @return
+     */
     public static byte[] updateLog(long xid, DataItem di) {
         byte[] logType = {LOG_TYPE_UPDATE};
         byte[] xidRaw = Parser.long2Byte(xid);
@@ -160,6 +228,11 @@ public class Recover {
         return Bytes.concat(logType, xidRaw, uidRaw, oldRaw, newRaw);
     }
 
+    /**
+     * 解析更新日志的信息
+     * @param log
+     * @return
+     */
     private static UpdateLogInfo parseUpdateLog(byte[] log) {
         UpdateLogInfo li = new UpdateLogInfo();
         li.xid = Parser.parseLong(Arrays.copyOfRange(log, OF_XID, OF_UPDATE_UID));
@@ -206,6 +279,13 @@ public class Recover {
     private static final int OF_INSERT_OFFSET = OF_INSERT_PGNO+4;
     private static final int OF_INSERT_RAW = OF_INSERT_OFFSET+2;
 
+    /**
+     * 插入一条日志
+     * @param xid
+     * @param pg
+     * @param raw
+     * @return
+     */
     public static byte[] insertLog(long xid, Page pg, byte[] raw) {
         byte[] logTypeRaw = {LOG_TYPE_INSERT};
         byte[] xidRaw = Parser.long2Byte(xid);
@@ -214,6 +294,11 @@ public class Recover {
         return Bytes.concat(logTypeRaw, xidRaw, pgnoRaw, offsetRaw, raw);
     }
 
+    /**
+     * 解析插入日志的信息
+     * @param log
+     * @return
+     */
     private static InsertLogInfo parseInsertLog(byte[] log) {
         InsertLogInfo li = new InsertLogInfo();
         li.xid = Parser.parseLong(Arrays.copyOfRange(log, OF_XID, OF_INSERT_PGNO));
