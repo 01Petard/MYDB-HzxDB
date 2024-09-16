91a02cbf6a6efc0320da216c42e9d507dabc87e5
01Petard <1520394133@qq.com>
2024-09-17 01:23:04 +0800

    fix: 修复pageIndex包的注释

--- src/main/java/top/guoziyang/mydb/backend/dm/pageIndex/PageIndex.java
+++ src/main/java/top/guoziyang/mydb/backend/dm/pageIndex/PageIndex.java
@@ -1,25 +1,25 @@
 package top.guoziyang.mydb.backend.dm.pageIndex;
 
+import top.guoziyang.mydb.backend.dm.pageCache.PageCache;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-import top.guoziyang.mydb.backend.dm.pageCache.PageCache;
-
 public class PageIndex {
     // 将一页划成40个区间
     private static final int INTERVALS_NO = 40;
-    private static final int THRESHOLD = PageCache.PAGE_SIZE / INTERVALS_NO;
+    private static final int THRESHOLD = PageCache.PAGE_SIZE / INTERVALS_NO;  // 每个区间的大小，默认0.2KB
 
-    private Lock lock;
-    private List<PageInfo>[] lists;
+    private final Lock lock;
+    private final List<PageInfo>[] lists;
 
     @SuppressWarnings("unchecked")
     public PageIndex() {
         lock = new ReentrantLock();
-        lists = new List[INTERVALS_NO+1];
-        for (int i = 0; i < INTERVALS_NO+1; i ++) {
+        lists = new List[INTERVALS_NO + 1];
+        for (int i = 0; i < INTERVALS_NO + 1; i++) {
             lists[i] = new ArrayList<>();
         }
     }
@@ -34,14 +34,19 @@ public class PageIndex {
         }
     }
 
+    /**
+     * 获取页面（算出区间号，直接取即可）
+     * @param spaceSize
+     * @return  PageInfo，其中包含页号和空闲空间大小的信息
+     */
     public PageInfo select(int spaceSize) {
         lock.lock();
         try {
             int number = spaceSize / THRESHOLD;
-            if(number < INTERVALS_NO) number ++;
-            while(number <= INTERVALS_NO) {
-                if(lists[number].size() == 0) {
-                    number ++;
+            if (number < INTERVALS_NO) number++;
+            while (number <= INTERVALS_NO) {
+                if (lists[number].isEmpty()) {
+                    number++;
                     continue;
                 }
                 return lists[number].remove(0);
--- src/main/java/top/guoziyang/mydb/backend/dm/pageIndex/PageInfo.java
+++ src/main/java/top/guoziyang/mydb/backend/dm/pageIndex/PageInfo.java
@@ -3,7 +3,6 @@ package top.guoziyang.mydb.backend.dm.pageIndex;
 public class PageInfo {
     public int pgno;
     public int freeSpace;
-
     public PageInfo(int pgno, int freeSpace) {
         this.pgno = pgno;
         this.freeSpace = freeSpace;
